848c53cac50255b7b36e494e8647423de839210f
Derek Charles <d.charles@red-tek.co.uk>
2020-05-08 12:55:37 +0000

    Move development VM scripts into own repo
    https://github.com/doctor-claw/mate-dev-env

--- infrastructure/development_environment/CreateDevEnvVM.bat
+++
@@ -1,6 +0,0 @@
-vagrant plugin list > tmpFile 
-set /p installedVagrantPlugins= < tmpFile 
-
-ECHO.%installedVagrantPlugins% | FIND /I "vagrant-vbguest">Nul && ( Echo.Guest additions plugin installed already ) || ( vagrant plugin install vagrant-vbguest )
-
-vagrant up
--- infrastructure/development_environment/MountPersistentDataDrive.sh
+++
@@ -1,5 +0,0 @@
-echo "$2  $1  ext4  defaults  0 0" | sudo tee -a /etc/fstab
-sudo mkdir $1
-sudo mount -a
-chown vagrant:vagrant $1
-chmod 777 $1
