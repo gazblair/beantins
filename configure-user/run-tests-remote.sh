71555118d130bd3129e6ff65e790c8ed8e886d09
gazblair <gazblair@hotmail.com>
2021-05-17 09:25:31 +0000

    Remove redundant configure-user

--- configure-user/run-tests-remote.sh
+++
@@ -1,7 +0,0 @@
-export REGISTRATION_URL_ROOT=https://umio5nkbfk.execute-api.us-east-1.amazonaws.com/Dev/ 
-export CHAT_URL_ROOT=wss://7smbc7hd42.execute-api.us-east-1.amazonaws.com/Prod
-export SAM_TEMPLATE_FILE=registration.yaml
-export SAM_STACK_NAME=BeanTinsUserAccount
-export TEST_MODE=remote
-
-./node_modules/cucumber/bin/cucumber-js
